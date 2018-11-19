5cdf1bb52461d281727a82de6532aef9add4ff74
Harald Billstein <harald.billstein@gmail.com>
2018-11-19 17:44:41 +0100

    init

---
+++ src/main/java/billstein/harald/leavinghome/LeavinghomeApplication.java
@@ -0,0 +1,12 @@
+package billstein.harald.leavinghome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavinghomeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LeavinghomeApplication.class, args);
+  }
+}
