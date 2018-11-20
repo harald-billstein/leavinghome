b7e743aeb1b2bb14a4d3eee440769c15d8ec2887
Harald Billstein <harald.billstein@gmail.com>
2018-11-20 23:05:59 +0100

    Request object added.

---
+++ src/main/java/billstein/harald/leavinghome/services/SonosRequest.java
@@ -0,0 +1,14 @@
+package billstein.harald.leavinghome.services;
+
+public class SonosRequest {
+
+  private boolean resourcesOff;
+
+  public boolean isResourcesOff() {
+    return resourcesOff;
+  }
+
+  public void setResourcesOff(boolean resourcesOff) {
+    this.resourcesOff = resourcesOff;
+  }
+}
