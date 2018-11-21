bebdd006df705936ca7c1ca92ab068ac139d0ead
Harald Billstein <harald.billstein@gmail.com>
2018-11-21 23:41:21 +0100

    toggle feature added

--- src/main/java/billstein/harald/leavinghome/services/SonosService.java
+++ src/main/java/billstein/harald/leavinghome/services/SonosService.java
@@ -3,19 +3,36 @@ package billstein.harald.leavinghome.services;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SonosService {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(SonosService.class);
+  private String pausePath = "http://192.168.0.183:5005/pauseall/";
+  private String resumePath = "http://192.168.0.183:5005/resumeall/";
+  private boolean toggle = false;
 
-  public ResponseEntity<SonosResponse> pauseAll() {
+
+  public ResponseEntity<SonosResponse> playPauseToggle() {
+    LOGGER.info("SonosService/playPauseToggle - accessed");
 
     Integer responseCode = null;
 
     try {
-      URL url = new URL("http://192.168.0.183:5005/pauseall/");
+      URL url;
+
+      if (toggle) {
+         url = new URL(pausePath);
+        toggle = false;
+      } else {
+        url = new URL(resumePath);
+        toggle = true;
+      }
+
       HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
       httpURLConnection.setRequestMethod("GET");
       responseCode = httpURLConnection.getResponseCode();
@@ -26,12 +43,12 @@ public class SonosService {
     SonosResponse sonosResponse = new SonosResponse();
     if (responseCode == 200) {
       sonosResponse.setSuccess(true);
-      sonosResponse.setMessage("And all is quiet");
+      sonosResponse.setMessage("Affirmative, Dave. I read you.");
 
     } else {
       sonosResponse.setSuccess(false);
-      sonosResponse.setMessage("Something went wrong!");
-      // TODO LOG
+      sonosResponse.setMessage("It can only be attributable to human error.");
+      LOGGER.error("COULD_NOT_ACCESS_RESOURCE EXCEPTION - " + responseCode);
     }
 
     return ResponseEntity.ok(sonosResponse);
